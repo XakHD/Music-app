ef109495b0fdba42920d1ffc4c98f9d28f5ac2ac
XakHD <zcheldercraftllc@gmail.com>
2023-10-02 21:34:43 -0400

    spotify-clone

---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+
+import SupabaseProvider from '@/providers/SupabaseProvider'
+import UserProvider from '@/providers/UserProvider'
+import ModalProvider from '@/providers/ModalProvider'
+
+import './globals.css'
+import { Figtree } from 'next/font/google'
+import Sidebar from '@/components/Sidebar'
+import ToasterProvider from '@/providers/ToasterProvider'
+import getSongsByUserId from '@/actions/getSongsByUserId'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Spotify Clone',
+  description: 'Listen to the music!',
+};
+
+export const revalidate = 0;
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const userSongs = await getSongsByUserId();
+
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ToasterProvider/>
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider />
+        <Sidebar songs={userSongs}>
+        {children}
+        </Sidebar>
+        
+        </UserProvider>
+        </SupabaseProvider>
+        </body>
+    </html>
+  )
+}
+
